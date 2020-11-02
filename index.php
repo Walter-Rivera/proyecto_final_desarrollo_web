964584b0116a03274c6c91c4234a53acfb29bcb3
Walter-Rivera <giovannirivera1994.1@gmail.com>
2020-11-01 19:49:11 -0600

    agregando la capa modelo para la gestión de peritos

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
 
     /*requiriendo el archivo el archivo usuario (controlador) */
     require_once "Controlador/usuario.controler.php";
+    require_once "Controlador/perito.controler.php";
+
 
     
     
@@ -13,6 +15,8 @@
 
      /*requiriendo el archivo el archivo usuario (Modelo) */
      require_once "Modelo/usuario.model.php";
+     require_once "Modelo/perito.model.php";
+
 
     
 
